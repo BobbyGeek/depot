c95d3ad1756a4f6254ac1dd5a546fcd208c69802
Bobby Griffith <bobby@bobbygeek.com>
2013-10-25 22:30:10 -0400

    Validation!

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -2,6 +2,15 @@ require 'test_helper'
 
 class ProductTest < ActiveSupport::TestCase
   fixtures :products
+  test "product is not valid without a unique title - i18n" do
+    product = Product.new(title: products(:ruby).title,
+      description: "yyy",
+      price: 1,
+      image_url: "fred.gif")
+    assert product.invalid?
+    assert_eaul [I18n.translate('erros.messages.take')],
+      product.errors[:title]
+  end
   test "product is not valid wihtout a unique title" do
     product = Product.new(title: products(:ruby).title,
       description: "yyy",
